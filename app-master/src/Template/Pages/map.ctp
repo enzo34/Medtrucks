8326b871db4200a734c3a1b41da6b06c9853a736
Marvin <marvin.avitabilebeweb@gmail.com>
2017-05-09 17:39:28 +0200

    Implémentation de la function zone 09/05/17 17h40

--- app-master/src/Template/Pages/map.ctp
+++ app-master/src/Template/Pages/map.ctp
@@ -1,28 +1,34 @@
 <?php
+
 $this->layout = false;
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
+    <head>
+<?= $this->Html->charset() ?>
+<?= $this->Html->css('map.css') ?>
 
-    <?= $this->Html->charset() ?>
+    </head>
+    <body>
 
- <?= $this->Html->css('map.css') ?>
- 
-    
-  </head>
-  <body>
-    <h3 id ="global">Medtrucks Map Demo</h3>
-    <div id ="global"></div>
-    <div id="map"></div>
+        <h3 id ="global">Medtrucks Map Demo</h3>
+        <div id ="global"></div>
+        <div id="map"></div>
 
+        <input type="text" name="adresse">
+        <input id="bouton" class="adresse" type="submit" value="Rechercher adresses">
 
-<input type="text" name="adresse">
-<input id="bouton" class="adresse" type="submit" value="Rechercher adresses">
+        <input type="number" name="km" >
+        <input id="zone" class="adresse" type="submit" value="Go">
 
 <?= $this->Html->script('jquery-3.2.1.js') ?>
+
 <?= $this->Html->script('script.js') ?>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBmZA1UMJ9zVYOMpsW_ohwEk1J2BmG11As&callback=initMap">
-    </script>
-  </body>
+<?= $this->Html->script('perimetre.js') ?>
+<?= $this->Html->script('ajax.js') ?>
+
+
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBmZA1UMJ9zVYOMpsW_ohwEk1J2BmG11As&callback=initMap">
+        </script>
+    </body>
--- app-master/webroot/js/script.js
+++ app-master/webroot/js/script.js
@@ -1,3 +1,5 @@
+//les variables sont instanciés
+var city ="lunel";
 var donnees = {
     "type": "FeatureCollection",
     "features": []
@@ -15,8 +17,9 @@ var map;
 var apigeolock="https://maps.googleapis.com/maps/api/geocode/json?address=";
 var apikey="&key=AIzaSyBmZA1UMJ9zVYOMpsW_ohwEk1J2BmG11As";
 var adresse ="";
-var lat
-var lng
+var lat;
+var lng;
+//cette fonction renvoie l'adresse en données GPS pour ainsi l'envoyer sur la map 
 function getgps(){
 	adresse = $("input[type='text']").val().replace(" ","+");
 
@@ -25,8 +28,8 @@ function getgps(){
 		url:apigeolock+adresse+apikey,
 		dataType:"json",
 		success: function (data){
-			lat= data.results[0].geometry.location.lat
-			lng= data.results[0].geometry.location.lng
+			lat= data.results[0].geometry.location.lat;
+			lng= data.results[0].geometry.location.lng;
 
 			maPosition.geometry.coordinates = [lng,lat];
 			map.data.addGeoJson(maPosition);
@@ -36,28 +39,25 @@ function getgps(){
 	});
 }
 
-
+//l'evenement du click sur le bouton execute la fonction getgps
 $("#bouton").click(function(){
 	getgps();
 
 });
 
-
+//la map ce centre sur le point lors du placement du point sur la map 
 function center(){
 	var pos = new google.maps.LatLng(lat,lng);
 	
 	map.setCenter(pos);
 
 }
-
+//fonction qui affiche la MAP
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: new google.maps.LatLng(0, 0),
-          	zoom: 5
+          	zoom: 2
         });
 }
 
-
-
-
-
+          
